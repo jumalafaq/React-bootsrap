fde9b2a720c4f5cc1f6edcde727b523048afa14c
jumalafaq <jumalafaq@gmail.com>
2020-08-05 23:46:11 +0200

    React-bootsrap-front

---
+++ src/About.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const About = () => <div>bla 1111</div>;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import { Home } from "./Home";
+import { About } from "./About";
+import { PriceList } from "./PriceList";
+import { NoMatch } from "./NoMatch";
+import { Layout } from "./components/Layout";
+import { NavigationBar } from "./components/NavigationBar";
+import { Jumbotron } from "./components/Jumbotron";
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NavigationBar />
+      <Jumbotron />
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/cennik" component={PriceList} />
+            <Route path="/o-nas" component={About} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </Layout>
+    </React.Fragment>
   );
 }
 
